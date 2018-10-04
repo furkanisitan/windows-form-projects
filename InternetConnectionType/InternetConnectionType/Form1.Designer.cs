630bff7004f67eff8f9a59ef515600520eed9dfc
Furkan Işıtan <furkan.isitan@bil.omu.edu.tr>
2018-10-04 06:03:18 +0300

    InternetConnectionType

---
+++ InternetConnectionType/InternetConnectionType/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace InternetConnectionType
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWifi = new System.Windows.Forms.Label();
+            this.lblEthernet = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ethernet Connection : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wifi Connection : ";
+            // 
+            // lblWifi
+            // 
+            this.lblWifi.AutoSize = true;
+            this.lblWifi.Location = new System.Drawing.Point(162, 51);
+            this.lblWifi.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblWifi.Name = "lblWifi";
+            this.lblWifi.Size = new System.Drawing.Size(26, 17);
+            this.lblWifi.TabIndex = 2;
+            this.lblWifi.Text = "- - -";
+            // 
+            // lblEthernet
+            // 
+            this.lblEthernet.AutoSize = true;
+            this.lblEthernet.Location = new System.Drawing.Point(162, 12);
+            this.lblEthernet.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblEthernet.Name = "lblEthernet";
+            this.lblEthernet.Size = new System.Drawing.Size(26, 17);
+            this.lblEthernet.TabIndex = 3;
+            this.lblEthernet.Text = "- - -";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(87, 82);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(101, 30);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 115);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblEthernet);
+            this.Controls.Add(this.lblWifi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check Internet Connection Type";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWifi;
+        private System.Windows.Forms.Label lblEthernet;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ InternetConnectionType/InternetConnectionType/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace InternetConnectionType
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            var result = CheckTypes();
+            lblWifi.Text = result[0].ToString();
+            lblEthernet.Text = result[1].ToString();
+        }
+
+        private static bool[] CheckTypes()
+        {
+            bool[] result = { false, false }; // 0 => wifi, 1 => ethernet
+            foreach (var ni in NetworkInterface.GetAllNetworkInterfaces().Where(ni => ni.OperationalStatus == OperationalStatus.Up))
+            {
+                var name = ni.Name.ToLower();
+                if (name.Contains("wi-fi"))
+                    result[0] = true;
+                if (name.Contains("ethernet"))
+                    result[1] = true;
+            }
+            return result;
+        }
+    }
+}
